c3e2411665e46d2981168fc9532db01e1ed6cbde
Qin <1878298255@qq.com>
2022-04-15 16:02:11 +0800

    更新后brass类

--- brass.h
+++ brass.h
@@ -9,31 +9,31 @@ private:
 	long acctNum;
 	double balance;
 public:
-	Brass(const std::string& s = "Nullbody", long n = -1, double bel = 0.0);
+	Brass(const std::string& s = "Nullbody", long n = -1, double bal = 0.0);
 	void Deposit(double amt);
 	virtual void Withdraw(double amt);
 	double Balance() const;
 	virtual void ViewAcct() const;
-	virtual ~Brass();
 };
 
-class Brassplus : public Brass;
+class BrassPlus : public Brass
 {
 private:
-	double maxLoan;
-	double rate;
-	double owesBank;
+    double maxLoan;
+    double rate;
+    double owesBank;
 public:
-	BrassPlus(const std::string& s = "Nullbody", long an = -1,
-		double bal = 0.0, double ml = 500,
-		double r = 0.11125);
-	BrassPlus(const Brass& ba, double ml = 500,
-		double r = 0.11125);
-	virtual void ViewAcct()const;
-	virtual void Withdraw(double amt);
-	void ResetMax(double m) { maxLoan = m; }
-	void ResetRate(double r) { rate = r; };
-	void ResetOwes() { owesBank = 0; }
-}
+    BrassPlus(const std::string& s = "Nullbody", long an = -1,
+        double bal = 0.0, double ml = 500,
+        double r = 0.11125);
+    BrassPlus(const Brass& ba, double ml = 500,
+        double r = 0.11125);
+
+    virtual void ViewAcct()const;
+    virtual void Withdraw(double amt);
+    void ResetMax(double m) { maxLoan = m; }
+    void ResetRate(double r) { rate = r; };
+    void ResetOwes() { owesBank = 0; }
+};
 
 #endif // !BRASS_H_
